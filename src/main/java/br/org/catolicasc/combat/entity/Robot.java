e28f863e7b53903b9d3a448be8a9b7d1649c9772
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2019-06-25 15:00:38 -0300

    Initial commit

---
+++ src/main/java/br/org/catolicasc/combat/entity/Robot.java
@@ -0,0 +1,16 @@
+package br.org.catolicasc.combat.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Robot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String status;
+}
